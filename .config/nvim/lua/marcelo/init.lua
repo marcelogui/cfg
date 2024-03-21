c869ded9eb1fe9f543d4336b5bfafaa172e31ac8
Marcelo Guimarães <marcelodivi@gmail.com>
2024-03-21 15:13:02 -0300

    feat: change default split to right and below

--- .config/nvim/lua/marcelo/init.lua
+++ .config/nvim/lua/marcelo/init.lua
@@ -3,3 +3,5 @@ require("marcelo.set")
 -- prevents status code 0 on terminal close
 -- See: https://github.com/neovim/neovim/issues/14986#issuecomment-902705190
 vim.cmd("autocmd TermClose * execute 'bdelete! ' . expand('<abuf>')")
+vim.o.splitright = true
+vim.o.splitbelow = true
