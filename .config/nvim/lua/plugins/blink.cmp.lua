a13d6722ef08d0228282edc756bcfe5b21d21cfe
Marcelo Guimaraes <marcelodivi@gmail.com>
2025-06-13 10:07:16 -0300

    Fix autocomplete breaking with multiple lines

---
+++ .config/nvim/lua/plugins/blink.cmp.lua
@@ -0,0 +1,9 @@
+return {
+  --  https://github.com/Saghen/blink.cmp/issues/1727#issuecomment-2867452782
+  {
+    "saghen/blink.cmp",
+    opts = {
+      fuzzy = { implementation = "lua" },
+    },
+  },
+}
